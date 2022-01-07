71bac4fe566cb9a854fbaa4fb47c4b0cc19d7d2a
Charles Stover <github@charlesstover.com>
2022-01-07 15:16:38 -0800

    resolves #4

--- types/cypress-code-coverage.d.ts
+++ types/cypress-code-coverage.d.ts
@@ -1,32 +1,8 @@
-// https://github.com/cypress-io/code-coverage/blob/master/task.js
 declare module '@cypress/code-coverage/task' {
-  export interface Config {
-    readonly env: Env;
-  }
-
-  export interface Env {
-    readonly codeCoverageTasksRegistered: boolean;
-  }
-
-  export interface Mode {
-    readonly isInteractive: boolean;
-  }
-
-  export interface On {
-    (event: 'task', tasks: Tasks): void;
-    (event: string): void;
-  }
-
-  export interface Tasks {
-    readonly combineCoverage: (sentCoverage: string) => null;
-    readonly coverageReport: () => Promise<string> | null;
-    readonly resetCoverage: (mode: Mode) => null;
-  }
-
+  import 'cypress';
   export default function registerCodeCoverageTasks(
-    on: On,
-    // `config` is mutable.
+    on: Cypress.PluginEvents,
     // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-    config: Config,
-  ): Config;
+    config: Cypress.PluginConfigOptions,
+  ): void;
 }
