448a439b35e8378b4e924547980c2fcbe2231cf7
Charles Stover <github@charlesstover.com>
2022-01-07 15:26:12 -0800

    resolves #4 (#10)

--- packages/application/cypress/plugins/index.ts
+++ packages/application/cypress/plugins/index.ts
@@ -1,11 +1,15 @@
-if (!process.env.NYC_REPORT_DIR) {
+/// <reference types="cypress" />
+import cypressCodeCoverageTask from '@cypress/code-coverage/task';
+
+if (typeof process.env.NYC_REPORT_DIR === 'undefined') {
   process.env.NYC_REPORT_DIR = 'cypress/coverage';
 }
 
-import type { Config, On } from '@cypress/code-coverage/task';
-import cypressCodeCoverageTask from '@cypress/code-coverage/task';
-
-export default function cypressPlugins(on: On, config: Config): Config {
+export default function cypressPlugins(
+  on: Cypress.PluginEvents,
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+  config: Cypress.PluginConfigOptions,
+): Cypress.PluginConfigOptions {
   cypressCodeCoverageTask(on, config);
   return config;
 }
