cb94944c4e2d8f6bdc2b6222eced5b3f2498be04
Charles Stover <github@charlesstover.com>
2022-02-23 11:23:40 -0600

    simplify react-application's eslint config

--- packages/react-application/.eslintrc.cjs
+++ packages/react-application/.eslintrc.cjs
@@ -1,42 +1,6 @@
 module.exports = {
-  plugins: ['jsx-a11y', 'react', 'react-hooks'],
-  root: false,
-
-  env: {
-    jest: true,
-    node: true,
-  },
-
-  extends: [
-    'plugin:jsx-a11y/strict',
-    'plugin:react/jsx-runtime',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    '@monorepo-template/react-fixable',
-    '@monorepo-template/react-strict',
-    '@monorepo-template/react-typescript',
-    '@monorepo-template/react-typescript/react-scripts-4',
-  ],
-
-  overrides: [
-    {
-      files: ['*.json'],
-      extends: ['@monorepo-template/react-typescript/json'],
-    },
-  ],
-
+  extends: ['@monorepo-template/react-module'],
   parserOptions: {
     tsconfigRootDir: __dirname,
-    useJSXTextNode: true,
-    warnOnUnsupportedTypeScriptVersion: false,
-    ecmaFeatures: {
-      jsx: false,
-    },
-  },
-
-  settings: {
-    react: {
-      version: 'detect',
-    },
   },
 };
