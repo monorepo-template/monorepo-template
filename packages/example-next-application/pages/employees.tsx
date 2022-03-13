93cb41953ae30979e780c3ece9a2c359f858d9fc
Charles Stover <github@charlesstover.com>
2022-03-13 00:07:53 -0800

    fix employees import

--- packages/example-next-application/pages/employees.tsx
+++ packages/example-next-application/pages/employees.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from 'react';
-import EmployeesFeature from '../src/features/home';
+import EmployeesFeature from '../src/features/employees';
 
 export default function EmployeesPage(): ReactElement {
   return <EmployeesFeature />;
