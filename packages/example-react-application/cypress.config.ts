321c850bb4e8753102c6e3e3ada3a13f14f4c2bf
Charles Stover <github@charlesstover.com>
2022-06-11 14:52:36 -0700

    update cypress config file type

---
+++ packages/example-react-application/cypress.config.ts
@@ -0,0 +1,12 @@
+export default {
+  baseUrl: 'http://localhost:3000/',
+  fixturesFolder: false,
+  integrationFolder: 'src',
+  projectId: '4akrvv',
+  redirectionLimit: 2,
+  testFiles: '**/*.e2e.ts',
+  env: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    NODE_ENV: 'development',
+  },
+};
