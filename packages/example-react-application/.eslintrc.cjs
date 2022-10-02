44912d17369402b5fa62bbd104c08e916a3652ec
Charles Stover <github@charlesstover.com>
2022-10-02 00:05:22 -0700

    fix Cypress tsconfig

--- packages/example-react-application/.eslintrc.cjs
+++ packages/example-react-application/.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@monorepo-template/eslint-config/react-module'],
   parserOptions: {
+    project: ['./tsconfig.eslint.json', './tsconfig.eslint.cypress.json'],
     tsconfigRootDir: __dirname,
   },
 };
